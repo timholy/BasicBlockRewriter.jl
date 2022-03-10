8a857a7075354270998997eb2130d56b2f454780
Tim Holy <tim.holy@gmail.com>
2022-03-10 12:43:48 -0600

    instantiate

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,6 @@ using Pkg
 @testset "BasicBlockRewriter.jl" begin
     exampledir = joinpath(dirname(@__DIR__), "examples")
     Pkg.activate(exampledir)
+    Pkg.instantiate()
     include(joinpath(exampledir, "fragment_counter.jl"))
 end
