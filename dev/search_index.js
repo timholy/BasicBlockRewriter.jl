012386f5a9fc52c9a27427fa8e8ecae041cc3d3b
Documenter.jl <documenter@juliadocs.github.io>
2022-03-10 10:33:01 +0000

    build based on ca474f2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BasicBlockRewriter","category":"page"},{"location":"#BasicBlockRewriter","page":"Home","title":"BasicBlockRewriter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BasicBlockRewriter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BasicBlockRewriter]","category":"page"}]
+}
