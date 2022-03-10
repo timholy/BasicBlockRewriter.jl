4ef9f6c099ac41082a4e1e465b4d4c257c4c188b
Tim Holy <tim.holy@gmail.com>
2022-03-10 12:48:36 -0600

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Pkg
 
 @testset "BasicBlockRewriter.jl" begin
     exampledir = joinpath(dirname(@__DIR__), "examples")
-    Pkg.activate(exampledir)
-    Pkg.instantiate()
+    # Pkg.activate(exampledir)
+    # Pkg.instantiate()
     include(joinpath(exampledir, "fragment_counter.jl"))
 end
